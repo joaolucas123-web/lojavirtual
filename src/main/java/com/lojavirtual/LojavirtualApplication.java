2c1dd6f9d974a94f0538e13d8c524217a3f2af66
joaolucas123 <vanellebmabc@gmail.com>
2021-04-19 20:17:08 -0300

    projeto criado

---
+++ src/main/java/com/lojavirtual/LojavirtualApplication.java
@@ -0,0 +1,18 @@
+package com.lojavirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojavirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojavirtualApplication.class, args);
+		
+		
+	    System.out.println("BANCO RODANDO");
+		
+		
+	}
+
+}
